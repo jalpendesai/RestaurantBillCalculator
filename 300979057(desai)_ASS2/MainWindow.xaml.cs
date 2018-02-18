d2543fea09a726015e0d2dc3a25c57a0d5f2fd40
jalpendesai <djalpen24@gmail.com>
2018-02-17 20:15:07 -0800

    Added Exporting to Excel File

--- 300979057(desai)_ASS2/MainWindow.xaml.cs
+++ 300979057(desai)_ASS2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
 
 namespace _300979057_desai__ASS2
 {
@@ -22,7 +26,7 @@ namespace _300979057_desai__ASS2
 
    
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Excel.Window
     {
         List<BillDetails> Beverage = new List<BillDetails>();
         List<BillDetails> Appetizer = new List<BillDetails>();
@@ -34,6 +38,7 @@ namespace _300979057_desai__ASS2
         public MainWindow()
         {
             InitializeComponent();
+
             Beverage.Add(new BillDetails {Name = "Soda", Category = "Beverage", Price = 1.95 });
             Beverage.Add(new BillDetails { Name = "Tea", Category = "Beverage", Price = 1.50 });
             Beverage.Add(new BillDetails { Name = "Coffee", Category = "Beverage", Price = 1.25 });
@@ -65,12 +70,7 @@ namespace _300979057_desai__ASS2
             Dessert.Add(new BillDetails { Name = "Mud Pie", Category = "Dessert", Price = 4.95 });
             Dessert.Add(new BillDetails { Name = "Apple Crisp", Category = "Dessert", Price = 5.95 });
 
-            //dgCustomer.Visibility = Visibility.Hidden;
-            //dgCustomer.ItemsSource = Beverage;
-
-            // Overwritting DataGrid value to Appetizer
-            //dgCustomer.ItemsSource = Appetizer
-
+            
             cbBeverage.DisplayMemberPath = "Name";
             cbBeverage.SelectedValuePath = "Price";
             cbBeverage.SetBinding(ComboBox.ItemsSourceProperty, new Binding() { Source = Beverage });
@@ -87,10 +87,7 @@ namespace _300979057_desai__ASS2
             cbDessert.SelectedValuePath = "Price";
             cbDessert.SetBinding(ComboBox.ItemsSourceProperty, new Binding() { Source = Dessert });
 
-
-
         }
-
 
 
         private void cbBeverage_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -236,6 +233,31 @@ namespace _300979057_desai__ASS2
 
         }
 
+        private void export_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true; //www.ahmetcansever.com
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+
+            for (int j = 0; j < dgCustomer.Columns.Count; j++) //Başlıklar için
+            {
+                Range myRange = (Range)sheet1.Cells[1, j + 1];
+                sheet1.Cells[1, j + 1].Font.Bold = true; //Başlığın Kalın olması için
+                sheet1.Columns[j + 1].ColumnWidth = 15; //Sütun genişliği ayarı
+                myRange.Value2 = dgCustomer.Columns[j].Header;
+            }
+            for (int i = 0; i < dgCustomer.Columns.Count; i++)
+            { //www.ahmetcansever.com
+                for (int j = 0; j < dgCustomer.Items.Count; j++)
+                {
+                    TextBlock b = dgCustomer.Columns[i].GetCellContent(dgCustomer.Items[j]) as TextBlock;
+                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                    myRange.Value2 = b.Text;
+                }
+            }
+        }
+
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             lblTest.Content = "$0.00";
@@ -255,6 +277,14 @@ namespace _300979057_desai__ASS2
 
         }
 
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if(dgCustomer.SelectedItem != null)
+            {
+                dgCustomer.SelectedItems.Remove(dgCustomer.SelectedItem);
+            }
+        }
+
         private void cmbSelectedChanged(ComboBox cmb)
         {
             //if(cmb.SelectedIndex != 1)
@@ -263,5 +293,158 @@ namespace _300979057_desai__ASS2
             //}
         }
 
+        dynamic Excel.Window.Activate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivateNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivatePrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Close(object SaveChanges, object Filename, object RouteWorkbook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic LargeScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Excel.Window NewWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic _PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintPreview(object EnableChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ScrollWorkbookTabs(object Sheets, object Position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SmallScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsX(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsY(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic RangeFromPoint(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScrollIntoView(int Left, int Top, int Width, int Height, object Start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Excel.Application Application => throw new NotImplementedException();
+
+        public XlCreator Creator => throw new NotImplementedException();
+
+        dynamic Excel.Window.Parent => throw new NotImplementedException();
+
+        public Range ActiveCell => throw new NotImplementedException();
+
+        public Chart ActiveChart => throw new NotImplementedException();
+
+        public Pane ActivePane => throw new NotImplementedException();
+
+        public dynamic ActiveSheet => throw new NotImplementedException();
+
+        public dynamic Caption { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayFormulas { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayGridlines { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHeadings { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHorizontalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayOutline { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool _DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayVerticalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayWorkbookTabs { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayZeros { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool EnableResize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool FreezePanes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int GridlineColor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlColorIndex GridlineColorIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Index => throw new NotImplementedException();
+
+        public string OnWindow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Panes Panes => throw new NotImplementedException();
+
+        public Range RangeSelection => throw new NotImplementedException();
+
+        public int ScrollColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ScrollRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Sheets SelectedSheets => throw new NotImplementedException();
+
+        public dynamic Selection => throw new NotImplementedException();
+
+        public bool Split { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitHorizontal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitVertical { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double TabRatio { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public XlWindowType Type => throw new NotImplementedException();
+
+        public double UsableHeight => throw new NotImplementedException();
+
+        public double UsableWidth => throw new NotImplementedException();
+
+        public bool Visible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Range VisibleRange => throw new NotImplementedException();
+
+        public int WindowNumber => throw new NotImplementedException();
+
+        XlWindowState Excel.Window.WindowState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public dynamic Zoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlWindowView View { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public SheetViews SheetViews => throw new NotImplementedException();
+
+        public dynamic ActiveSheetView => throw new NotImplementedException();
+
+        public bool DisplayRuler { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AutoFilterDateGrouping { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool DisplayWhitespace { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Hwnd => throw new NotImplementedException();
     }
 }
